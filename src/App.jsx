41b27c38132f6373108d3db017aa0f805bd4af27
Priti201 <pritigupta161169@gmail.com>
2024-01-08 10:13:49 +0530

    created app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+
+
+import './App.css';
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app-wrapper">
+      <div className="app-container">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
