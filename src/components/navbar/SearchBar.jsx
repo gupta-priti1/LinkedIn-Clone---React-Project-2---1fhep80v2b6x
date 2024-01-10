5bd7dd6f2c277d4c090ef4263f1835c7874e6ffe
Priti201 <pritigupta161169@gmail.com>
2024-01-10 09:09:14 +0530

    created basic structure

--- src/components/navbar/SearchBar.jsx
+++ src/components/navbar/SearchBar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { TextField } from "@mui/material";
 
+import SearchIcon from "@mui/icons-material/Search";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import GroupIcon from "@mui/icons-material/Group";
+import WorkIcon from "@mui/icons-material/Work";
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className="searchbar-container">
+      <div>
+        <SearchIcon />
+        <TextField id="filled-basic" label="Search" variant="filled" />
+      </div>
+      <div>
+        <div>
+          <HomeRoundedIcon />
+          <h4>Home</h4>
+        </div>
+        <div>
+          <GroupIcon />
+          <h4>Network</h4>
+        </div>
+        <div>
+          <WorkIcon />
+          <h4>Jobs</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
