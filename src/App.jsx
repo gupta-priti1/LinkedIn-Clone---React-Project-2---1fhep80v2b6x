ca40a345ff74a6a78f1ca662671f62f9dd0a0357
Priti201 <pritigupta161169@gmail.com>
2024-01-12 18:43:05 +0530

    temp chnges

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import React from "react";
 
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
+import Routers from "./Routes/Routers";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <div className="app-container">
-        <Navbar />
+       <Routers/>
       </div>
     </div>
   );
