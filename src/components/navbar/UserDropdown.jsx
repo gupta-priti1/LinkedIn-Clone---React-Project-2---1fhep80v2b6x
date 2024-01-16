2dfd137a7f1f4a6ecde128171bfdbdaf761cb672
Priti201 <pritigupta161169@gmail.com>
2024-01-16 23:57:38 +0530

    added wrapper

--- src/components/navbar/UserDropdown.jsx
+++ src/components/navbar/UserDropdown.jsx
@@ -4,25 +4,27 @@ import { Dropdown } from "@mui/base/Dropdown";
 import { MenuButton } from "@mui/base/MenuButton";
 import { Menu } from "@mui/base/Menu";
 import { MenuItem } from "@mui/base/MenuItem";
+import { styled } from '@mui/system';
 import { Avatar } from "@mui/material";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { DropDownWrapper} from "../Styles/Wrapper";
 
 const UserDropdown = () => {
   return (
-    <div>
+    <DropDownWrapper>
       <div className="user-photo">
-        <Avatar sx={{ width:25, height: 25, marginTop: '2px', backgroundColor:'#0A66C2',color:'white !important', fontSize:12}}>OP</Avatar>
+        <Avatar sx={{ width:25, height: 25, marginTop: '2px', backgroundColor:'#0A66C2',color:'white !important', fontSize:12}}>R</Avatar>
       </div>
       <Dropdown>
-        <MenuButton className="menu-btn">Me <ArrowDropDownIcon/></MenuButton>
-        <Menu>
-          <MenuItem>Profile</MenuItem>
-          <MenuItem>Try Premium</MenuItem>
-          <MenuItem>Dark Mode</MenuItem>
-          <MenuItem>Sign out</MenuItem>
+        <MenuButton className="menu-btn">Me<ArrowDropDownIcon/></MenuButton>
+        <Menu className="menu-items">
+          <DropDownWrapper className="menu-items">Profile</DropDownWrapper>
+          <DropDownWrapper>Try Premium</DropDownWrapper>
+          <DropDownWrapper>Dark Mode</DropDownWrapper>
+          <DropDownWrapper>Sign out</DropDownWrapper>
         </Menu>
       </Dropdown>
-    </div>
+    </DropDownWrapper>
   );
 };
 
