28e4b06761c50d56101cc60469671ff6ef4f6caa
Priti201 <pritigupta161169@gmail.com>
2024-01-16 19:23:50 +0530

    fixed flex direction

--- src/components/Styles/Wrapper.js
+++ src/components/Styles/Wrapper.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
+  display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   width: 100%;
   background-color: white;
-`
+`;
