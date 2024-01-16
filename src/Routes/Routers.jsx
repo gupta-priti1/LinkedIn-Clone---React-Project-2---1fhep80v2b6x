2a5b24d08506d7da44c83d4adf4430302cb63e99
Priti201 <pritigupta161169@gmail.com>
2024-01-16 19:10:05 +0530

    created routes for navbar links

--- src/Routes/Routers.jsx
+++ src/Routes/Routers.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
+import MyNetwork from '../pages/MyNetwork'
+import Jobs from '../pages/Jobs'
+import Messaging from '../pages/Messaging'
+import Notifications from '../pages/Notifications'
 
 const Routers = () => {
   return (
@@ -8,6 +12,10 @@ const Routers = () => {
     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/feed' element={<Home/>}/>
+        <Route path='/mynetwork' element={<MyNetwork/>}/>
+        <Route path='/jobs' element={<Jobs/>}/>
+        <Route path='/messaging' element={<Messaging/>}/>
+        <Route path='/notifications' element={<Notifications/>}/>
     </Routes>
     </>
   )
