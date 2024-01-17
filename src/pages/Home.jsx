fb928fbb214ff8461d4cef7228c238f055704cd9
Priti201 <pritigupta161169@gmail.com>
2024-01-17 19:14:24 +0530

    created basic fillefor home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Wrapper } from '../components/Styles/Wrapper';
+import UserInfo from '../components/Home/UserInfo';
+import Post from '../components/Home/Post';
+import News from '../components/Home/News';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Wrapper>
+      <UserInfo/>
+      <Post/>
+      <News/>
+    </Wrapper>
   )
 }
 
