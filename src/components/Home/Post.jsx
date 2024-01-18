1b8e201d00677c06ee9a1851d714f9fdf917f81d
Priti201 <pritigupta161169@gmail.com>
2024-01-18 23:48:53 +0530

    created post layout

--- src/components/Home/Post.jsx
+++ src/components/Home/Post.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { PostContainer } from '../Styles/Style'
+import CreatePost from './CreatePost'
+import DisplayPost from './DisplayPost'
 
 const Post = () => {
   return (
-    <PostContainer>Post</PostContainer>
+    <PostContainer>
+      <CreatePost/>
+      <DisplayPost/>
+    </PostContainer>
   )
 }
 
