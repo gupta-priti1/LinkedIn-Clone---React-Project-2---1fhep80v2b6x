02ced73488a4e0afc4e9fe8e0df288cfc36eb43b
Priti201 <pritigupta161169@gmail.com>
2024-01-22 00:03:45 +0530

    imported required icons

--- src/components/Home/Reactions.jsx
+++ src/components/Home/Reactions.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Wrapper } from '../Styles/Wrapper'
-import { FlexContainer } from '../Styles/Style'
+import React from "react";
+import { Wrapper } from "../Styles/Wrapper";
+import { FlexContainer } from "../Styles/Style";
+
+import { FaThumbsUp } from "react-icons/fa";
+import { BsChatText } from "react-icons/bs";
+import { BiRepost } from "react-icons/bi";
+import { RiSendPlaneFill } from "react-icons/ri";
 
 const Reactions = () => {
   return (
     <Wrapper>
-        
-        <FlexContainer>
-            
-        </FlexContainer>
+      <FlexContainer>
+        <FaThumbsUp />
+        <h4>Like</h4>
+      </FlexContainer>
+      <FlexContainer>
+        <BsChatText />
+        <h4>Comment</h4>
+      </FlexContainer>
+      <FlexContainer>
+        <BiRepost />
+        <h4>Repost</h4>
+      </FlexContainer>
+      <FlexContainer>
+        <RiSendPlaneFill />
+        <h4>Send</h4>
+      </FlexContainer>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Reactions
+export default Reactions;
