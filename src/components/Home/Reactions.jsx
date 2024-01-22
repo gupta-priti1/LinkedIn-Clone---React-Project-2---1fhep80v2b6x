6d02f20f5d0fbf408c34f0e1641202c40c64385c
Priti201 <pritigupta161169@gmail.com>
2024-01-22 23:34:32 +0530

    fixed styling of reaction buttons

--- src/components/Home/Reactions.jsx
+++ src/components/Home/Reactions.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Wrapper } from "../Styles/Wrapper";
-import { FlexContainer } from "../Styles/Style";
+import { FlexContainer, ReactionsContainer, SingleReaction } from "../Styles/Style";
 
 import { FaThumbsUp } from "react-icons/fa";
+import { FaRegThumbsUp } from "react-icons/fa";
 import { BsChatText } from "react-icons/bs";
 import { BiRepost } from "react-icons/bi";
 import { RiSendPlaneFill } from "react-icons/ri";
@@ -10,22 +11,25 @@ import { RiSendPlaneFill } from "react-icons/ri";
 const Reactions = () => {
   return (
     <Wrapper>
-      <FlexContainer>
-        <FaThumbsUp />
-        <h4>Like</h4>
-      </FlexContainer>
-      <FlexContainer>
-        <BsChatText />
-        <h4>Comment</h4>
-      </FlexContainer>
-      <FlexContainer>
-        <BiRepost />
-        <h4>Repost</h4>
-      </FlexContainer>
-      <FlexContainer>
-        <RiSendPlaneFill />
-        <h4>Send</h4>
-      </FlexContainer>
+      <ReactionsContainer>
+        <SingleReaction>
+          {/* <FaThumbsUp className="icon"/> */}
+          <FaRegThumbsUp className="icon"/>
+          <h4>Like</h4>
+        </SingleReaction>
+        <SingleReaction>
+          <BsChatText />
+          <h4>Comment</h4>
+        </SingleReaction>
+        <SingleReaction>
+          <BiRepost className="repost-icon"/>
+          <h4>Repost</h4>
+        </SingleReaction>
+        <SingleReaction>
+          <RiSendPlaneFill />
+          <h4>Send</h4>
+        </SingleReaction>
+      </ReactionsContainer>
     </Wrapper>
   );
 };
