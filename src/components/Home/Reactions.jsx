1267ed4e084d3d6649a5e99cc4bc954cfb7a7ca6
Priti201 <pritigupta161169@gmail.com>
2024-01-23 18:16:15 +0530

    created function for liking a post

--- src/components/Home/Reactions.jsx
+++ src/components/Home/Reactions.jsx
@@ -7,12 +7,28 @@ import { FaRegThumbsUp } from "react-icons/fa";
 import { BsChatText } from "react-icons/bs";
 import { BiRepost } from "react-icons/bi";
 import { RiSendPlaneFill } from "react-icons/ri";
+import axios from "axios";
 
 const Reactions = () => {
+
+  const handleLikeClick = async()=>{
+    console.log('liked');
+    const response = await axios.post(
+      'https://academics.newtonschool.co/api/v1/linkedin/like/65ab4d67277339d884512a41',
+      '',
+      {
+        headers: {
+          'projectID': 'hv45l4abtvvc',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWVhMWY1MmUyMWUyZjk3ZmVjMDM5NiIsImlhdCI6MTcwNTk0MzU0MSwiZXhwIjoxNzM3NDc5NTQxfQ.czAeNFN7xxc1ocRkvDlHlDJubmZ6mCGYAkgAFA4UM7w'
+        }
+      }
+    );
+    console.log(response);
+  }
   return (
     <Wrapper>
       <ReactionsContainer>
-        <SingleReaction>
+        <SingleReaction onClick={handleLikeClick}>
           {/* <FaThumbsUp className="icon"/> */}
           <FaRegThumbsUp className="icon"/>
           <h4>Like</h4>
