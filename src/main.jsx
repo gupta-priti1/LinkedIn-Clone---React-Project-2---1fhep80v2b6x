375169d9db442fe0490ce6dbde89ae2c155b428b
Priti201 <pritigupta161169@gmail.com>
2024-01-23 18:28:00 +0530

    set up for post context api

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import PostContext from "./components/context/PostContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PostContext>
+        <App />
+      </PostContext>
     </BrowserRouter>
   </React.StrictMode>
 );
