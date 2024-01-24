b6a2f6186e084f06a0574e966a1afa80736d07ec
Priti201 <pritigupta161169@gmail.com>
2024-01-24 23:55:05 +0530

    implemented logic for fetching comments

--- src/components/Home/Comments.jsx
+++ src/components/Home/Comments.jsx
@@ -1,12 +1,32 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CommentInput, FlexContainer } from "../Styles/Style";
 import UserImage from "../userImage/UserImage";
+import axios from "axios";
 
-const Comments = () => {
-    const handleFetchingComments = ()=>{
+const Comments = ({ props }) => {
+  const [comments, setComments] = useState();
+  useEffect(() => {
+    handleFetchingComments();
+  }, []);
 
+  const postId = props.postId;
+
+  const handleFetchingComments = async () => {
+    // console.log("comment");
+    const response = await axios.get(
+      `https://academics.newtonschool.co/api/v1/linkedin/post/${postId}/comments`,
+      {
+        headers: {
+          projectID: "hv45l4abtvvc",
+        },
+      }
+    );
+    console.log(response);
+    if (response.status === 200) {
+      setComments(response.data.data);
     }
+  };
+  console.log("comments", comments);
   return (
     <div>
       <FlexContainer>
@@ -14,7 +34,12 @@ const Comments = () => {
         <CommentInput />
       </FlexContainer>
 
+      {comments?.map((comment) => {
+        return(
 
+            <div>{comment.content}</div>
+        )
+      })}
     </div>
   );
 };
