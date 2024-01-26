a795a85e4ed3ae334548f15a2c1e24e9cf1b447d
Priti201 <pritigupta161169@gmail.com>
2024-01-26 19:55:49 +0530

    implemented logic for fetching search terms

--- src/components/navbar/SearchBar.jsx
+++ src/components/navbar/SearchBar.jsx
@@ -1,15 +1,55 @@
-import React from "react";
-import { Input } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
+import { Input } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+
 import { SearchInput, SearchbarContainer } from "../Styles/Style";
 
+import axios from "axios";
+
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      fetchingSearchResult();
+    }, 800);
+    return ()=>clearTimeout(timer);
+  }, [searchTerm]);
+
+  const fetchingSearchResult = async () => {
+    if(searchTerm === '')
+    {
+      return
+    }
+    try {
+      const response = await axios.get(
+        `https://academics.newtonschool.co/api/v1/linkedin/post?search={"author.name":"${searchTerm}","content":"${searchTerm}","title":"${searchTerm}"}`,
+        {
+          headers: {
+            projectID: "hv45l4abtvvc",
+          },
+        }
+      );
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSearchInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <SearchbarContainer>
       <SearchIcon />
       <SearchInput>
-        <Input placeholder="Search" fullWidth />
+        <Input
+          placeholder="Search"
+          fullWidth
+          onChange={(e) => handleSearchInput(e)}
+        />
       </SearchInput>
     </SearchbarContainer>
   );
