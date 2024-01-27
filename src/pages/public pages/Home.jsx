3b7aa682b5c1eec6e20f424712198d9cb71b7e60
Priti201 <pritigupta161169@gmail.com>
2024-01-27 15:19:34 +0530

    removed error

--- src/pages/public pages/Home.jsx
+++ src/pages/public pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { HomeWrapper, Wrapper } from "../../components/Styles/Wrapper";
 import UserInfo from "../../components/Home/UserInfo";
 import Post from "../../components/Home/post/Post";
 import News from "../../components/Home/News";
