cb03ec1fc6738578032dfd869ebd634e48a178f5
Priti201 <pritigupta161169@gmail.com>
2024-01-27 15:20:12 +0530

    imported the add component

--- src/pages/public pages/search display pages/SearchDisplay.jsx
+++ src/pages/public pages/search display pages/SearchDisplay.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Temp2 from "../../../components/Home/Temp2";
 
 const SearchDisplay = () => {
   return (
-    <div>SearchDisplay</div>
-  )
-}
+    <div>
+      SearchDisplay
+      <Temp2 />
+    </div>
+  );
+};
 
-export default SearchDisplay
+export default SearchDisplay;
