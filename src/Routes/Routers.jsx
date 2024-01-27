8ced979b4d8045969a92eabff5decf54012943c7
Priti201 <pritigupta161169@gmail.com>
2024-01-27 15:14:17 +0530

    arranged loaction of files

--- src/Routes/Routers.jsx
+++ src/Routes/Routers.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import MyNetwork from '../pages/MyNetwork'
-import Jobs from '../pages/Jobs'
-import Messaging from '../pages/Messaging'
-import Notifications from '../pages/Notifications'
-import SearchDisplay from '../pages/SearchDisplay'
-import {RoutesWrapper } from '../components/Styles/Wrapper'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/public pages/Home";
+import MyNetwork from "../pages/public pages/MyNetwork";
+import Jobs from "../pages/public pages/Jobs";
+import Messaging from "../pages/public pages/Messaging";
+import Notifications from "../pages/public pages/Notifications";
+import SearchDisplay from "../pages/public pages/search display pages/SearchDisplay";
+import { RoutesWrapper } from "../components/Styles/Wrapper";
 
 const Routers = () => {
   return (
     <RoutesWrapper>
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/feed' element={<Home/>}/>
-        <Route path='/mynetwork' element={<MyNetwork/>}/>
-        <Route path='/jobs' element={<Jobs/>}/>
-        <Route path='/messaging' element={<Messaging/>}/>
-        <Route path='/notifications' element={<Notifications/>}/>
-        <Route path='/search/:searchTerm' element={<SearchDisplay/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/feed" element={<Home />} />
+        <Route path="/mynetwork" element={<MyNetwork />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/messaging" element={<Messaging />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/search/:searchTerm" element={<SearchDisplay />} />
+      </Routes>
     </RoutesWrapper>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;
---
+++ src/pages/public pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { HomeWrapper, Wrapper } from "../../components/Styles/Wrapper";
+import UserInfo from "../../components/Home/UserInfo";
+import Post from "../../components/Home/post/Post";
+import News from "../../components/Home/News";
+import { HomeContainer } from "../../components/Styles/Style";
+
+const Home = () => {
+  return (
+    <>
+      <HomeContainer>
+        <UserInfo />
+        <Post />
+        <News />
+      </HomeContainer>
+    </>
+  );
+};
+
+export default Home;
