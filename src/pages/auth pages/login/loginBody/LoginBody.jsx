b12c467cb8f6bba4c1f6a5e79fb1f9cc6d127166
Priti201 <pritigupta161169@gmail.com>
2024-01-28 23:34:26 +0530

    created structure

--- src/pages/auth pages/login/loginBody/LoginBody.jsx
+++ src/pages/auth pages/login/loginBody/LoginBody.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import LoginWlcText from "./LoginWlcText";
 import LoginForm from "./LoginForm";
+import { Image } from "react-bootstrap";
+
+import SideImage from "./../../../../assets/Images/Linkedin-login.png";
+import { LoginBodyContainer } from "../../../../components/Styles/LoginStyle";
 
 const LoginBody = () => {
   return (
-    <>
-      <LoginWlcText />
-      <LoginForm/>
-      
-    </>
+    <LoginBodyContainer>
+      <div>
+        <LoginWlcText />
+        <LoginForm />
+      </div>
+
+      <Image src={SideImage} />
+    </LoginBodyContainer>
   );
 };
 
--- src/pages/auth pages/login/loginBody/LoginForm.jsx
+++ src/pages/auth pages/login/loginBody/LoginForm.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import ButtonComponent from "../../../../components/button/ButtonComponent";
+// import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <LoginForm>
+        <label>
+            Email:
+        </label>
+        <input type="email" required/>
+        <label>
+            Password:
+        </label>
+        <input type="password" required/>
+        <ButtonComponent text="Login"/>
+    </LoginForm>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
