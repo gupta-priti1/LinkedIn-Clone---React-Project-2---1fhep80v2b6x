6470d13a56825b316f17db8feaf44fd80df6cffe
Priti201 <pritigupta161169@gmail.com>
2024-01-30 00:00:38 +0530

    created structure for signup page

--- src/pages/auth pages/signup/SignupBody.jsx
+++ src/pages/auth pages/signup/SignupBody.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { LoginText } from '../../../components/Styles/LoginStyle'
 import { SignupText } from '../../../components/Styles/SignupStyle'
+import SignupForm from './SignupForm'
 
 const SignupBody = () => {
   return (
     <div>
     <SignupText>Make the most of your professional life</SignupText>
+    <SignupForm/>
     </div>
   )
 }
