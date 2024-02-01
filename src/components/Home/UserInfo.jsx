721f0f9fa92f44e31a84f485f1aaaf1dfd9e283c
Priti201 <pritigupta161169@gmail.com>
2024-02-02 00:11:44 +0530

    imported image for background

--- src/components/Home/UserInfo.jsx
+++ src/components/Home/UserInfo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { UserInfoContainer } from '../Styles/Style'
+import { Image } from 'react-bootstrap'
+
+import Background from "./../../assets/Images/user-background.png"
 
 const UserInfo = () => {
   return (
-    <UserInfoContainer>UserInfo</UserInfoContainer>
+    <UserInfoContainer>
+      <Image src={Background} width={'100%'}/>
+    </UserInfoContainer>
   )
 }
 
