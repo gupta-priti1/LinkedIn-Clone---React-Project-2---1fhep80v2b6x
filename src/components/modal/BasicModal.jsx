c2a1ced0ebdb737d4e0e00631ff4a27bad1f2870
Priti201 <pritigupta161169@gmail.com>
2024-02-02 22:39:34 +0530

    implemented api logic for creating a post

--- src/components/modal/BasicModal.jsx
+++ src/components/modal/BasicModal.jsx
@@ -53,10 +53,12 @@ export default function BasicModal() {
           title: postDetails.title,
           content: postDetails.description,
           images: postDetails.images,
+          // Content-Type: "multipart/form-data" 
         },
         {
           headers: {
             projectID: "hv45l4abtvvc",
+            "Content-Type": "multipart/form-data",
             Authorization:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWVhMWY1MmUyMWUyZjk3ZmVjMDM5NiIsImlhdCI6MTcwNTk0MzU0MSwiZXhwIjoxNzM3NDc5NTQxfQ.czAeNFN7xxc1ocRkvDlHlDJubmZ6mCGYAkgAFA4UM7w",
           },
