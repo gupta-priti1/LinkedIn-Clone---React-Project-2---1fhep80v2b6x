2c2508a017ed71de4a19d34ab27038743beb3e0e
Priti201 <pritigupta161169@gmail.com>
2024-02-08 23:47:25 +0530

    added no result found

--- src/pages/public pages/search display pages/SearchDisplay.jsx
+++ src/pages/public pages/search display pages/SearchDisplay.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { SearchDisplayWrapper } from "../../../components/Styles/Wrapper";
 import PostContainer from "../../../components/postContainer/PostContainer";
 import { searchItemsContext } from "../../../components/context/SearchItemsContext";
-import News from "../../../components/Home/News";
 import Temp2 from "../../../components/Home/advertisement/Temp2";
+import { Image } from "react-bootstrap";
+
+import NoProduct from "./../../../assets/Images/no-search-found.webp"
 
 const SearchDisplay = () => {
   const { searchItems, setSearchItems } = searchItemsContext();
+  if(searchItems.length === 0)
+  {
+    return(
+      <div>
+        <Image src={NoProduct}/>
+      </div>
+    )
+  }
   return (
     <SearchDisplayWrapper>
       <PostContainer posts={searchItems} />
