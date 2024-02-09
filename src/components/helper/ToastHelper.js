fa343c58ee3ca86405d7b51af947a28ccb211cb8
Priti201 <pritigupta161169@gmail.com>
2024-02-09 08:21:57 +0530

    created helper function for toast messages

---
+++ src/components/helper/ToastHelper.js
@@ -0,0 +1,45 @@
+import { toast } from "react-toastify";
+
+const successToastOptions = {
+  position: "top-right",
+  autoClose: 1000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  theme: "dark",
+};
+const errorToatOptions = {
+  position: "bottom-right",
+  autoClose: 1000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  theme: "dark",
+};
+const infoToastOptions = {
+  position: "top-center",
+  autoClose: 1000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  theme: "dark",
+};
+function ToasterMessage(type, message) {
+  switch (type) {
+    case "success":
+      toast.success(message, successToastOptions);
+      break;
+    case "error":
+      toast.error(message, errorToatOptions);
+      break;
+    case "info":
+      toast(message, infoToastOptions);
+
+    default:
+      break;
+  }
+}
+export { ToasterMessage };
