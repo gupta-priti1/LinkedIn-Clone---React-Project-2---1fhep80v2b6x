078c4353664758fdfc939923c118843d2b549945
Priti201 <pritigupta161169@gmail.com>
2024-02-09 09:33:36 +0530

    added lazy loading and alt to images

--- src/components/userImage/UserImage.jsx
+++ src/components/userImage/UserImage.jsx
@@ -6,11 +6,23 @@ const UserImage = ({ userImageStyling, profileImage, name }) => {
   return (
     <>
       {profileImage ? (
-        <Image src={profileImage} style={{...userImageStyling, borderRadius:"50%", cursor:"pointer"}}/>
+        <Image
+          src={profileImage}
+          alt="profile-image"
+          loading="lazy"
+          style={{
+            ...userImageStyling,
+            borderRadius: "50%",
+            cursor: "pointer",
+          }}
+        />
       ) : (
         <div className="user-photo">
-          <Avatar sx={userImageStyling} style={{ border: "3.5px solid white", cursor:"pointer"}}>
-          {name?.charAt(0).toUpperCase()}
+          <Avatar
+            sx={userImageStyling}
+            style={{ border: "3.5px solid white", cursor: "pointer" }}
+          >
+            {name?.charAt(0).toUpperCase()}
           </Avatar>
         </div>
       )}
