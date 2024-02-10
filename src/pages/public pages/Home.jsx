f769d50728ffcd03bea82ec4befa8e9af6f3129f
Priti201 <pritigupta161169@gmail.com>
2024-02-10 16:43:38 +0530

    added access check

--- src/pages/public pages/Home.jsx
+++ src/pages/public pages/Home.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserInfo from "../../components/Home/UserInfo";
 import Post from "../../components/Home/post/Post";
 import News from "../../components/Home/News";
 import { HomeContainer } from "../../components/Styles/Style";
+import { accessTokenApi } from "../../components/context/AccessTokenContext";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { accessToken } = accessTokenApi();
+  // const accessToken = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
+
+  // console.log(accessToken, "accessToken");
+  useEffect(() => {
+    if (accessToken === "") {
+      navigate("/login");
+      return;
+    }
+  }, []);
+
   return (
     <>
       <HomeContainer>
