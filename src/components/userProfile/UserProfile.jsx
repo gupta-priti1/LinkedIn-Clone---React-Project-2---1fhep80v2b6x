604a90ea9659b95d042f4d687cbb86384181611c
Priti201 <pritigupta161169@gmail.com>
2024-02-10 17:17:33 +0530

    added authorization check

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserDetails from "./UserDetails";
 import { UserProfileWrapper } from "../Styles/ProfileStyle";
 import Temp2 from "../Home/news/advertisement/Temp2";
+import { accessTokenApi } from "../context/AccessTokenContext";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile = () => {
+  const { accessToken } = accessTokenApi();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken === "") {
+      navigate("/login");
+      return;
+    }
+  }, []);
+
   return (
     <UserProfileWrapper>
       <UserDetails />
