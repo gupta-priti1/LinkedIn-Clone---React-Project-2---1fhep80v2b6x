f94f3157898c873518de40dcd34ad16def37341f
Priti201 <pritigupta161169@gmail.com>
2024-02-10 17:13:02 +0530

    added authorization check

--- src/pages/underConstruction pages/UnderConstruction.jsx
+++ src/pages/underConstruction pages/UnderConstruction.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Image } from "react-bootstrap";
 
 import underConstructionImg from "./../../assets/Images/under-con.jpg";
 import { FlexContainer } from "../../components/Styles/Style";
+import { accessTokenApi } from "../../components/context/AccessTokenContext";
+import { useNavigate } from "react-router-dom";
 const UnderConstruction = () => {
+  const { accessToken } = accessTokenApi();
+  // const accessToken = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken === "") {
+      navigate("/login");
+      return;
+    }
+  }, []);
   return (
     <FlexContainer>
       <Image
